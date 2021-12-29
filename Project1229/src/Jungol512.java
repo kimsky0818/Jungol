a278e2ad1647dfc7d8324f461b9a36378cfadea2
Si7A-00 <Si7A-00@Si7A-09>
2021-12-29 10:45:46 +0900

    1st commit

---
+++ Project1229/src/Jungol512.java
@@ -0,0 +1,13 @@
+public class Jungol512 {
+
+	public static void main(String[] args) {
+		
+		int weight = 49;
+		double gravity = 0.2683;
+		
+//		System.out.print(weight + "*" gravity + "=" + weight * gravity);
+//		System.out.println();
+		System.out.printf("%d * %f = %f\n" , weight,gravity,weight*gravity);
+	}
+
+}
